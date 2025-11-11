5a34c8b3316ff82a8e5649b43ec3d5f8a1c9437d
krishnapatel-1 <krishna9329ptl@gmail.com>
2025-11-11 11:30:20 +0530

    glass effect

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -3,42 +3,18 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="glass-navbar">
       <div className="navbar-container">
 
-        {/* LEFT SIDE LOGO */}
-        <div className="navbar-logo">
-          <NavLink to="/">Krishna Patel</NavLink>
+        <div className="nav-logo">
+          <NavLink to="/">Krishna</NavLink>
         </div>
 
-        {/* RIGHT SIDE LINKS */}
-        <ul className="navbar-links">
-          <li>
-            <NavLink 
-              to="/" 
-              className={({ isActive }) => isActive ? "active-link" : ""}
-            >
-              Home
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink 
-              to="/project" 
-              className={({ isActive }) => isActive ? "active-link" : ""}
-            >
-              Project
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink 
-              to="/work" 
-              className={({ isActive }) => isActive ? "active-link" : ""}
-            >
-              Work
-            </NavLink>
-          </li>
+        <ul className="nav-links">
+          <li><NavLink to="/" className={({isActive}) => isActive ? "active-link" : ""}>Home</NavLink></li>
+          <li><NavLink to="/project" className={({isActive}) => isActive ? "active-link" : ""}>Projects</NavLink></li>
+          <li><NavLink to="/work" className={({isActive}) => isActive ? "active-link" : ""}>Work</NavLink></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
 
       </div>
